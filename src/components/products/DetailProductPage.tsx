1d6e9397d1bf5231fd892314b510233d43bb7ed2
ParkSeongJun <sjyj98@naver.com>
2024-08-12 19:11:07 +0900

    redux store 추가

--- src/components/products/DetailProductPage.tsx
+++ src/components/products/DetailProductPage.tsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import useSWR from 'swr'
 import { useSearchParams } from 'react-router-dom'
+import { addCart } from '../../store/cartSlice'
+import { useAppDispatch } from '../../store/store'
 
 interface DetailProduct {
     id: number,
@@ -14,6 +16,8 @@ interface DetailProduct {
 
 const DetailProductPage = () => {
 
+    const dispatch = useAppDispatch();
+
     const [quantity, setQuantity] = useState(1);
 
     const [searchParams] = useSearchParams();
@@ -23,6 +27,18 @@ const DetailProductPage = () => {
         const response = await fetch(url);
         return response.json();
     });
+
+    const checkQuantity = () => {
+        if (quantity < 1) {
+            alert('수량은 1개 이상이어야 합니다.');
+            setQuantity(1);
+        }
+    }
+
+
+    useEffect(() => {
+        checkQuantity();
+    }, [quantity]);
 
 
   return (
@@ -35,6 +51,16 @@ const DetailProductPage = () => {
                 <h3>{data.title}</h3>
                 <p>{data.price} $</p>
                 <p>{data.description}</p>
+                <QuantityContainer>
+                    <p>quantity : </p>
+                    <button onClick={() => setQuantity(quantity - 1)}>-</button>
+                    <span>{quantity}</span>
+                    <button onClick={() => setQuantity(quantity + 1)}>+</button>
+                </QuantityContainer>
+                <ButtonContainer>
+                    <button onClick={()=>dispatch(addCart({name: data.name, price: data.price, quantity: quantity}))}>장바구니 담기</button>
+                    <button>구매하기</button>
+                </ButtonContainer>
             </ProductContainer>
         )}
     </Container>
@@ -48,6 +74,51 @@ export default DetailProductPage
 const Container = styled.div`
     margin-top: 50px;
     width: 100%;
+`;
+
+const QuantityContainer = styled.div`
+    display: flex;
+    width: 200px;
+    justify-content: space-between;
+    align-items: center;
+    margin:10px auto;
+    button {
+        width: 30px;
+        height: 30px;
+        border: none;
+        border-radius: 5px;
+        background-color: lightgray;
+        cursor: pointer;
+        transition: all 0.3s;
+
+        &:hover {
+            background-color: gray;
+            color: white;
+            scale: 1.1;
+        }
+    }
+    `;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    width: 250px;
+    justify-content: space-between;
+    margin-top: 20px;
+    button {
+        width: 100px;
+        height: 40px;
+        border: none;
+        border-radius: 5px;
+        background-color: lightgray;
+        cursor: pointer;
+        transition: all 0.3s;
+
+        &:hover {
+            background-color: gray;
+            color: white;
+            scale: 1.1;
+        }
+    }
 `;
 
 const ProductContainer = styled.div`
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch } from "react-redux";
-import { cartSlice } from "./cartSlice";
-import { useSelector } from "react-redux";
-
-export const store = configureStore({
-    reducer: { cart : cartSlice.reducer },
-});
